3d264ae84a72bb68cf6776d4dadf915835cc4558
Mercedes Magnelli <mechamagnelli@gmail.com>
2021-05-11 20:49:52 -0300

    Arreglo type test mediante refactor de movimientos

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -3,16 +3,15 @@ package dds.monedero.model;
 import java.time.LocalDate;
 
 public class Movimiento {
+
   private LocalDate fecha;
   //En ningún lenguaje de programación usen jamás doubles para modelar dinero en el mundo real
   //siempre usen numeros de precision arbitraria, como BigDecimal en Java y similares
   private double monto;
-  private boolean esDeposito;
 
-  public Movimiento(LocalDate fecha, double monto, boolean esDeposito) {
+  public Movimiento(LocalDate fecha, double monto) {
     this.fecha = fecha;
     this.monto = monto;
-    this.esDeposito = esDeposito;
   }
 
   public double getMonto() {
@@ -23,36 +22,28 @@ public class Movimiento {
     return fecha;
   }
 
-  public boolean fueDepositado(LocalDate fecha) {
-    return isDeposito() && esDeLaFecha(fecha);
-  }
-
-  public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
+  public boolean fueOperado(LocalDate fecha) {
+    return esDeLaFecha(fecha);
   }
 
   public boolean esDeLaFecha(LocalDate fecha) {
     return this.fecha.equals(fecha);
   }
 
-  public boolean isDeposito() {
-    return esDeposito;
-  }
-
-  public boolean isExtraccion() {
-    return !esDeposito;
-  }
 
   public void agregateA(Cuenta cuenta) {
     cuenta.setSaldo(calcularValor(cuenta));
-    cuenta.agregarMovimiento(fecha, monto, esDeposito);
+    cuenta.agregarMovimiento(fecha, monto);
   }
 
-  public double calcularValor(Cuenta cuenta) {
+  /*public double calcularValor(Cuenta cuenta) {
     if (esDeposito) {
       return cuenta.getSaldo() + getMonto();
     } else {
       return cuenta.getSaldo() - getMonto();
     }
   }
+
+}
+   */
 }
