8435bd173266bcc11294d56bd4a5d28aac146b3f
Mercedes Magnelli <mechamagnelli@gmail.com>
2021-05-11 21:40:32 -0300

    elimino un método innecesario y se elimina un misplaced method

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -32,10 +32,7 @@ public class Movimiento {
   }
 
 
-  public void agregateA(Cuenta cuenta) {
-    cuenta.setSaldo(calcularValor(cuenta));
-    cuenta.agregarMovimiento(fecha, monto);
-  }
+
 
   /*public double calcularValor(Cuenta cuenta) {
     if (esDeposito) {
